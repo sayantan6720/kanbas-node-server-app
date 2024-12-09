2cb4df4029bad336966e193408ac7b6c9c0f09fc
Sayantan Datta <sayantandatta275@gmail.com>
2024-12-08 21:57:02 -0500

    first commit in a6

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
@@ -8,6 +10,10 @@ import "dotenv/config";
 import CourseRoutes from "./kanbas/Courses/routes.js";
 import ModuleRoutes from "./kanbas/Modules/routes.js";
 import AssignmentRoutes from "./kanbas/Assignments/routes.js";
+
+const CONNECTION_STRING =
+  process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas";
+mongoose.connect(CONNECTION_STRING);
 const app = express();
 app.use(
   cors({
